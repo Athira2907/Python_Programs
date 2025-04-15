f832d0287ce46953f97460611feb3b11f03a8a8c
Athira_Krishnan <148784084+Athira2907@users.noreply.github.com>
2025-04-16 00:18:05 +0530

    Update med_1.py

--- med_1.py
+++ med_1.py
@@ -152,4 +152,7 @@ for i in l1:
     print(str(i[0])+' '+str(len(i)))
     c+=1
 
-10) 
+10) https://www.hackerrank.com/challenges/triangle-quest-2/problem?isFullScreen=true
+for i in range(1,int(input())+1): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print(int(''.join([str(i) for i in range(1,i+1)]+[str(i) for i in range(i-1,0,-1)])))
+    
